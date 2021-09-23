65c6ee83bdfd417e784f074e6db4f345df9591c6
frank ndungu <ndungufrank01@gmail.com>
2021-09-23 21:41:49 +0300

    commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Todo from "./components/Todo";
+
+function App() {
+  return (
+    <div>
+      <h1>My Todos</h1>
+      <Todo text="Learn React" />
+      <Todo text="Learn React Native" />
+      <Todo text="Master React" />
+    </div>
+  );
+}
+
+export default App;
